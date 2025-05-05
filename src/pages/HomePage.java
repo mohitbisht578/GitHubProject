4066e422fd0a6a349a45ce9ed71d83a55b191d56
mohitbisht578 <mohitbisht578@gmail.com>
2025-05-05 16:48:51 +0530

    first commit

---
+++ src/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my hello home page");
+	}
+}
---
+++ src/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my hello login page");
+	}
+}
