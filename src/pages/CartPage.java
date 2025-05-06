6003456665949bc71718fd30e41a2088244b9c04
mohitbisht578 <mohitbisht578@gmail.com>
2025-05-06 09:13:03 +0530

    cart page added

---
+++ src/pages/CartPage.java
@@ -0,0 +1,9 @@
+package src.pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("cart page");
+	}
+}
--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -4,6 +4,7 @@ public class SearchPage {
 
 	public void search() {
 		System.out.println("search");
+		String str = "mohit";
 	}
 
 }
