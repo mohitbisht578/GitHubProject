1ad5a8ca55cdb6a714799a4ac060f639281b36d9
mohitbisht578 <70063201+mohitbisht578@users.noreply.github.com>
2025-05-07 09:02:25 +0530

    Created RegistrationPage.java

---
+++ src/pages/RegistrationPage.java
@@ -0,0 +1,11 @@
+package src.pages;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("RegistrationPage");
+
+		System.out.println("RegistrationPage feature");
+	}
+}
